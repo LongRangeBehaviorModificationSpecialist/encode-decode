59779ee4334767fe421b4b441240f7c57a045964
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-06-19 11:31:18 -0400

    Update base64_input.py

--- base64_input.py
+++ base64_input.py
@@ -12,10 +12,12 @@ class Base64:
         self.results = results
         self.data_type = data_type
 
+
     def base64_to_ascii(self, input_string: str) -> str:
         """Convert BASE64 string to ASCII string."""
         ascii_string = base64.b64decode(input_string).decode()
         return ascii_string
+
 
     def base64_to_binary(self, input_string: str) -> str:
         """Convert BASE64 string to BINARY string."""
@@ -24,17 +26,20 @@ class Base64:
                 input_string).decode())
         return binary_string
 
+
     def base64_to_decimal(self, input_string: str) -> str:
         """Convert BASE64 string to DECIMAL string."""
         d = [ord(i) for i in base64.b64decode(input_string).decode()]
         dec_result = " ".join(str(x) for x in d)
         return dec_result
 
+
     def base64_to_hexadecimal(self, input_string: str) -> str:
         """Convert BASE64 string to HEXADECIMAL string."""
         decoded_bytes = base64.b64decode(input_string)
         spaced_result = " ".join(f"{n:02x}" for n in decoded_bytes)
         return spaced_result.upper()
+
 
     def make_data_dict(self, input_string: str) -> None:
         self.results["type"] = "Base64"
@@ -50,6 +55,7 @@ class Base64:
         morse_code = MorseCode.encode_morse_code(self, input_string)
         self.results["Morse Code"] = f"{morse_code}"
         return self.results
+
 
     def print_base64_output(self, input_string: str) -> None:
         output = self.make_data_dict(input_string)
