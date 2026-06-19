0c5bc1f071cbfb0013fa754a84a7628c642fbf67
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-06-19 11:30:37 -0400

    Update ascii_input.py

--- ascii_input.py
+++ ascii_input.py
@@ -14,10 +14,12 @@ class ASCII:
         self.results = results
         self.data_type = data_type
 
+
     def ascii_to_base64(self, input_string: str) -> str:
         """Convert the ASCII input string to BASE64 string."""
         b64_string = base64.b64encode(input_string.encode()).decode()
         return b64_string
+
 
     def ascii_to_binary(self, input_string: str) -> str:
         """Convert the ASCII input string to BINARY string."""
@@ -28,38 +30,43 @@ class ASCII:
         binary_string = " ".join(x for x in s)
         return binary_string
 
+
     def ascii_to_decimal(self, input_string: str) -> str:
         """Convert the ASCII input string to DECIMAL string."""
         decimal_string = " ".join(
             str(x) for x in [ord(i) for i in input_string])
         return decimal_string
 
+
     def ascii_to_hexadecimal(self, input_string: str) -> str:
         """Convert the ASCII input string to HEXADECIMAL string."""
         hex_string = " ".join(f"{ord(c):02X}" for c in input_string)
         return hex_string
+
 
     def ascii_to_rot13(self, input_string: str) -> str:
         """Convert the ASCII input string to ROT13 string."""
         rot13_string = codecs.encode(input_string, "rot_13")
         return rot13_string
 
+
     def make_data_dict(self, input_string: str) -> dict:
         self.results["type"] = "ascii"
         self.results["user_input"] = f"{input_string}"
-        b64 = self.ascii_to_base64(input_string)
-        self.results["Base64"] = f"{b64}"
-        binary = self.ascii_to_binary(input_string)
-        self.results["Binary"] = f"{binary}"
-        decimal = self.ascii_to_decimal(input_string)
-        self.results["Decimal"] = f"{decimal}"
-        hex = self.ascii_to_hexadecimal(input_string)
-        self.results["Hexadecimal"] = f"{hex}"
-        rot13 = self.ascii_to_rot13(input_string)
-        self.results["Rot13"] = f"{rot13}"
-        morse_code = MorseCode.encode_morse_code(self, input_string)
-        self.results["Morse Code"] = f"{morse_code}"
+        # b64 = self.ascii_to_base64(input_string)
+        self.results["Base64"] = f"{self.ascii_to_base64(input_string)}"
+        # binary = self.ascii_to_binary(input_string)
+        self.results["Binary"] = f"{self.ascii_to_binary(input_string)}"
+        # decimal = self.ascii_to_decimal(input_string)
+        self.results["Decimal"] = f"{self.ascii_to_decimal(input_string)}"
+        # hex = self.ascii_to_hexadecimal(input_string)
+        self.results["Hexadecimal"] = f"{self.ascii_to_hexadecimal(input_string)}"
+        # rot13 = self.ascii_to_rot13(input_string)
+        self.results["Rot13"] = f"{self.ascii_to_rot13(input_string)}"
+        # morse_code = MorseCode.encode_morse_code(self, input_string)
+        self.results["Morse Code"] = f"{MorseCode.encode_morse_code(self, input_string)}"
         return self.results
+
 
     def print_ascii_output(self, input_string: str) -> None:
         output = self.make_data_dict(input_string)
