6d8f9c463ba7d45bcc994549d4b6b2fe861d55d1
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-07-26 18:20:12 -0400

    Update base64_input.py

--- base64_input.py
+++ base64_input.py
@@ -15,41 +15,32 @@ class Base64:
 
 
     def base64_to_ascii(self) -> str:
-        """
-        Convert base64 string to ascii string.
-        """
+        """Convert base64 string to ascii string."""
         return base64.b64decode(self.input_string).decode()
 
 
     def base64_to_base32(self) -> str:
-        """
-        Convert a base64 string to a base32 string.
-        """
+        """Convert a base64 string to a base32 string."""
         raw_bytes = base64.b64decode(self.input_string)
         return base64.b32encode(raw_bytes).decode("ascii")
 
 
     def base64_to_binary(self) -> str:
-        """
-        Convert base64 string to binary string.
-        """
+        """Convert base64 string to binary string."""
         return " ".join(
             format(ord(c), "b").zfill(8) for c in base64.b64decode(
-                self.input_string).decode())
+                self.input_string).decode()
+        )
 
 
     def base64_to_decimal(self) -> str:
-        """
-        Convert base64 string to decimal string.
-        """
+        """Convert base64 string to decimal string."""
         d = [ord(c) for c in base64.b64decode(self.input_string).decode()]
         return " ".join(str(x) for x in d)
 
 
     def base64_to_hexadecimal(self) -> str:
-        """
-        Convert base64 string to hexadecimal string.
-        """
+        """Convert base64 string to hexadecimal string."""
         decoded_bytes = base64.b64decode(self.input_string)
         return " ".join(f"{n:02x}" for n in decoded_bytes).upper()
 
